7a5de14e792998507e7247dd86bb2fbba320126f
s098031 <tests098031@gmail.com>
2023-12-25 02:16:18 +0800

    20231225

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "112_1_mmslab_java_final_project"
+include(":app")
