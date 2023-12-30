ee3e7d3d42cf804a376ef660de65129fb6992021
s098031 <tests098031@gmail.com>
2023-12-31 04:15:18 +0800

    20231231_vr1.0

---
+++ app/src/main/java/com/example/a112_1_mmslab_java_final_project/GodGame.java
@@ -0,0 +1,14 @@
+package com.example.a112_1_mmslab_java_final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GodGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_god_game);
+    }
+}
